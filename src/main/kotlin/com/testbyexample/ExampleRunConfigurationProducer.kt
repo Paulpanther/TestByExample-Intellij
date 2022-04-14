0e4b3046f801b56b1a94c81a1e16810bc14b205c
Paul Methfessel <paul.methfessel@web.de>
2022-04-14 17:31:20 +0200

    Create Gradle Run Config

--- src/main/kotlin/com/testbyexample/ExampleRunConfigurationProducer.kt
+++ src/main/kotlin/com/testbyexample/ExampleRunConfigurationProducer.kt
@@ -5,24 +5,40 @@ import com.intellij.execution.actions.LazyRunConfigurationProducer
 import com.intellij.execution.configurations.ConfigurationFactory
 import com.intellij.openapi.util.Ref
 import com.intellij.psi.PsiElement
+import com.intellij.psi.util.parentOfType
 import org.jetbrains.kotlin.psi.KtAnnotationEntry
+import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.KtNamedFunction
+import org.jetbrains.plugins.gradle.service.execution.GradleExternalTaskConfigurationType
+import org.jetbrains.plugins.gradle.service.execution.GradleRunConfiguration
 
-class ExampleRunConfigurationProducer: LazyRunConfigurationProducer<GradleConfig>() {
+class ExampleRunConfigurationProducer: LazyRunConfigurationProducer<GradleRunConfiguration>() {
 
     override fun setupConfigurationFromContext(
-        configuration: JUnitConfiguration,
+        configuration: GradleRunConfiguration,
         context: ConfigurationContext,
         sourceElement: Ref<PsiElement>
     ): Boolean {
-        val element = sourceElement.get()
-        val annotation = element as? KtAnnotationEntry ?: return false
+        val path = getFunctionPath(sourceElement.get()) ?: return false
 
+        configuration.name = path
+        configuration.settings.taskNames = listOf("test")
+        configuration.settings.scriptParameters = "--tests $path"
+        configuration.settings.externalProjectPath = context.project.basePath
         return true
     }
 
-    override fun isConfigurationFromContext(configuration: JUnitConfiguration, context: ConfigurationContext): Boolean {
-        return false
+    override fun isConfigurationFromContext(configuration: GradleRunConfiguration, context: ConfigurationContext): Boolean {
+        val element = context.location?.psiElement ?: return false
+        val path = getFunctionPath(element) ?: return false
+        return configuration.name == path
     }
 
-    override fun getConfigurationFactory(): ConfigurationFactory = JUnitConfigurationType().configurationFactories.first()
+    private fun getFunctionPath(element: PsiElement): String? {
+        if (element.text != "Example") return null
+        val funParent = element.parentOfType<KtNamedFunction>()
+        return funParent?.fqName?.asString()
+    }
+
+    override fun getConfigurationFactory(): ConfigurationFactory = GradleExternalTaskConfigurationType.getInstance().factory
 }
